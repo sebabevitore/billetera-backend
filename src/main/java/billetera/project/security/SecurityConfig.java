8717e0b7a70917342fd3cbc7d3bf5e711c5a228c
sebabevitore <sbevitore@uade.edu.ar>
2026-08-09 12:12:33 -0300

    cors settings

--- src/main/java/billetera/project/security/SecurityConfig.java
+++ src/main/java/billetera/project/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://billetin.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
